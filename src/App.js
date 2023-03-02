099a0e0216b35cc0535cb94c1517f675cf49e6b5
황준서 <1201q@naver.com>
2023-03-02 20:45:13 +0900

    firebase 로그인

--- src/App.js
+++ src/App.js
@@ -3,26 +3,42 @@ import List from "./Frontend/Pages/List";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { authService } from "./firebase";
+import { authService, dbService } from "./firebase";
 import {
   getRedirectResult,
   GoogleAuthProvider,
   signInWithRedirect,
   signOut,
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
+import {
+  addDoc,
+  doc,
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  getDocs,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 const queryClient = new QueryClient();
 
+//https://firebase.google.com/docs/auth/web/start?hl=ko&authuser=0
 // Main
 function App() {
   const [login, setLogin] = useState();
   const [userData, setUserData] = useState(null);
 
+  const [id, setID] = useState("");
+  const [pw, setPW] = useState("");
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        console.log(user);
+        console.log(user.uid);
         setLogin(true);
         setUserData(user);
       } else {
@@ -30,7 +46,40 @@ function App() {
         setUserData(null);
       }
     });
+
+    // //전부 가져옴
+    // onSnapshot(query(collection(dbService, "siba")), (q) => {
+    //   q.forEach((doc) => {
+    //     console.log(doc.data());
+    //   });
+    // });
+
+    // 특정한 유저만 가져옴
+    onSnapshot(doc(dbService, "siba", "kbvqheO4hkTccxvYw353"), (doc) => {
+      console.log(doc.data());
+    });
   }, []);
+
+  const emailLogin = async (e) => {
+    e.preventDefault();
+    const auth = getAuth();
+
+    // // 이메일 회원가입
+    // createUserWithEmailAndPassword(auth, id, pw)
+    //   .then((userData) => {
+    //     console.log(userData);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    // 로그인
+    signInWithEmailAndPassword(auth, id, pw)
+      .then((userData) => {
+        console.log(userData);
+      })
+      .catch((error) => console.log(error));
+  };
 
   const socialLogin = async () => {
     try {
@@ -52,8 +101,37 @@ function App() {
     return;
   };
 
+  async function add() {
+    await addDoc(collection(dbService, "siba"), {
+      text: "sibad",
+    });
+  }
+
+  async function update() {
+    await updateDoc(doc(dbService, "siba", "kbvqheO4hkTccxvYw353"), {
+      text: "업뎃dd ",
+    });
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
+      <form onSubmit={emailLogin}>
+        <input
+          type="email"
+          value={id}
+          onChange={(e) => {
+            setID(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          value={pw}
+          onChange={(e) => {
+            setPW(e.target.value);
+          }}
+        />
+        <input type="submit" value="회원가입" />
+      </form>
       <button
         onClick={() => {
           socialLogin();
@@ -69,6 +147,13 @@ function App() {
         로그아웃
       </button>
       {userData && userData.email}
+      <button
+        onClick={() => {
+          update();
+        }}
+      >
+        저장소
+      </button>
       <div className="App">
         <BrowserRouter>
           <Routes>
